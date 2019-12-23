ea10b16aecf6ad456779245265ebdbe0c412271c
kiritoxja <978760693@qq.com>
2019-12-23 15:41:09 +0800

    initial

---
+++ src/main/java/com/xja/springbootsns/SpringbootsnsApplication.java
@@ -0,0 +1,13 @@
+package com.xja.springbootsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xja/springbootsns/SpringbootsnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xja.springbootsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
