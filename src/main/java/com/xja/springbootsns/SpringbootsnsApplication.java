f3cb652786bc31716c20415ab8cb7c6ca4a42711
kiritoxja <978760693@qq.com>
2020-01-12 13:43:04 +0800

    登录注册功能

--- src/main/java/com/xja/springbootsns/SpringbootsnsApplication.java
+++ src/main/java/com/xja/springbootsns/SpringbootsnsApplication.java
@@ -13,5 +13,4 @@ public class SpringbootsnsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootsnsApplication.class, args);
     }
-
 }
--- src/test/java/com/xja/springbootsns/SpringbootsnsApplicationTests.java
+++ src/test/java/com/xja/springbootsns/SpringbootsnsApplicationTests.java
@@ -1,8 +1,11 @@
 package com.xja.springbootsns;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class SpringbootsnsApplicationTests {
@@ -12,5 +15,4 @@ class SpringbootsnsApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
